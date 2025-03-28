420090bba5c8f5ced6ccf7cf9c3005c48cb0658a
Avinash sharma <sharmaavi2196@gmail.com>
2025-03-29 02:11:02 +0530

    add initial components for menu, cart, and order features; implement routing and user creation

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,29 @@
 import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./ui/Home";
+import Menu from "./features/menu/Menu";
+import Cart from "./features/cart/Cart";
+import Order from "./features/order/Order";
+import CreateOrder from "./features/order/CreateOrder";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/menu",
+    element: <Menu />,
+  },
+  {
+    path: "cart",
+    element: <Cart />,
+  },
+  { path: "/order/new", element: <CreateOrder /> },
+  { path: "/order/:id", element: <Order /> },
+]);
 
 const App = () => {
-  return <div>helo vite!</div>;
+  return <RouterProvider router={router} />;
 };
 
 export default App;
