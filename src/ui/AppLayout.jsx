5cae47e336fc3d0a450794d22c572e951b4e021b
Avinash sharma <sharmaavi2196@gmail.com>
2025-03-30 18:26:34 +0530

    refactor routing structure and add AppLayout component; update CartOverview to use Link

---
+++ src/ui/AppLayout.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Header from "./Header";
+import CartOverview from "../features/cart/CartOverview";
+import { Outlet } from "react-router-dom";
+
+const AppLayout = () => {
+  return (
+    <div>
+      <Header />
+
+      <main>
+        <h1>content</h1>
+        <Outlet />
+      </main>
+
+      <CartOverview />
+    </div>
+  );
+};
+
+export default AppLayout;
