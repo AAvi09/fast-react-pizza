4dee185416b0aaa8cbaca435d9d16baf75233f5b
Avinash sharma <sharmaavi2196@gmail.com>
2025-04-02 22:10:20 +0530

    add order loader and error handling; implement search order feature with navigation

--- src/features/order/SearchOrder.jsx
+++ src/features/order/SearchOrder.jsx
@@ -1,9 +1,17 @@
-import { useState } from "react";
+import { useState, useNavigate } from "react";
 
 const Searchorder = () => {
   const [query, setQuery] = useState(" ");
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!query) return;
+    navigate(`/order/${query}`);
+    setQuery("");
+  };
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <input
         placeholder="Search order #"
         value={query}
