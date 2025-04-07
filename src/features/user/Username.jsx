896ed235d1dbff0a80324dbc83decebe556f2f9a
Avinash sharma <sharmaavi2196@gmail.com>
2025-04-07 09:19:18 +0530

    feat: enhance layout and visibility in CartOverview, Header, and Home components; update Username component for responsive display

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Username = () => {
-  return <div className="text-sm font-semibold">Avinash</div>;
+  return <div className="hidden text-sm font-semibold md:block">Avinash</div>;
 };
 
 export default Username;
--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -5,7 +5,7 @@ import Username from "../features/user/UserName";
 
 const Header = () => {
   return (
-    <header className="bg-yellow-500 px-4 py-3 uppercase border-b border-stone-300 sm:px-6">
+    <header className="bg-yellow-500 px-4 py-3 uppercase border-b border-stone-300 sm:px-6 flex items-center justify-between">
       <Link to="/" className="tracking-widest">
         Fast React Pizza Co.
       </Link>
