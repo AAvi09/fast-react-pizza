80dc04fa8bbcf79f670e8261607a3de184e40520
Avinash sharma <sharmaavi2196@gmail.com>
2025-04-19 10:11:33 +0530

    feat: enhance CartOverview to display total quantity and price; add selectors for cart data

--- src/features/cart/CartSlice.jsx
+++ src/features/cart/CartSlice.jsx
@@ -57,3 +57,9 @@ export const {
 } = CartSlice.actions;
 
 export default CartSlice.reducer;
+
+export const getTotalCartQuantity = (state) =>
+  state.cart.cart.reduce((total, item) => total + item.quantity, 0);
+
+export const getTotalCartPrice = (state) =>
+  state.cart.cart.reduce((sum, item) => sum + item.totalPrice, 0);
