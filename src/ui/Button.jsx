1bcf2ff88c8b3b36a540947b55b4f034ecfb722e
Avinash sharma <sharmaavi2196@gmail.com>
2025-04-19 09:11:32 +0530

    feat: implement add to cart functionality in MenuItem component; update Button component to support onClick prop

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const Button = ({ children, disabled, to }) => {
+const Button = ({ children, disabled, to, onClick }) => {
   const className =
     "bg-yellow-400 uppercase font-semibold text-stone-800 py-3 px-4  rounded-md hover:bg-yellow-500 hover:text-stone-100 focus:outline-none focus:ring-2 focus:ring-yellow-500 focus:ring-offset-2 focus:ring-offset-stone-200 inline-block cursor-pointer tracking-wide disabled:cursor-not-allowed";
   if (to)
@@ -9,6 +9,12 @@ const Button = ({ children, disabled, to }) => {
       <Link to={to} className={className}>
         {children}
       </Link>
+    );
+  if (onClick)
+    return (
+      <button onClick={onClick} className={className}>
+        {children}
+      </button>
     );
   return (
     <button disabled={disabled} className={className}>
