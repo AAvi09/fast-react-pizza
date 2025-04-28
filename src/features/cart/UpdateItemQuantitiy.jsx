0915bb75ced8b61bef7f4f288ac393014c371687
Avinash sharma <sharmaavi2196@gmail.com>
2025-04-28 21:43:16 +0530

    feat: add UpdateItemQuantity component for adjusting item quantities in the cart

---
+++ src/features/cart/UpdateItemQuantitiy.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const UpdateItemQuantitiy = () => {
+  return (
+    <div>
+      <button className="px-4 py-4">-</button>
+      <button className="px-4 py-4">+</button>
+    </div>
+  );
+};
+
+export default UpdateItemQuantitiy;
